c92f527f1547e5592808725f0ed1f790cf9052b2
Luke Shepard <luke@esparklearning.com>
2016-12-01 23:33:29 -0700

    Completed the first day! Test driven development, although the puts debugging was tougher than I'd hoped.

--- app/day1.rb
+++ app/day1.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'set'
+
 # --- Day 1: No Time for a Taxicab ---
 #
 # Santa's sleigh uses a very high-precision clock to guide its movements, and the clock's
@@ -27,15 +30,78 @@
 # R5, L5, R5, R3 leaves you 12 blocks away.
 # How many blocks away is Easter Bunny HQ?
 
+# --- Part Two ---
+#
+# Then, you notice the instructions continue on the back of the Recruiting Document. 
+# Easter Bunny HQ is actually at the first location you visit twice.
+#
+# For example, if your instructions are R8, R4, R4, R8, the first location you visit 
+# twice is 4 blocks away, due East.
+#
+# How many blocks away is the first location you visit twice?
+
 class Day1
-  def self.shortest_path(input)
+
+  def self.shortest_path_to_first_place_to_visit_twice(directions)
     
+    path = coordinates([0,0],
+                       [0,1],
+                       directions.map {|s| s.gsub(/ +/, "")})
 
+    visited = Set.new
+    path.each do |coord|
+      return coord[0].abs + coord[1].abs if visited.include?(coord)
+      visited.add(coord)
+    end
+  end
+  
+  def self.shortest_path_from_origin(directions)
 
+    path = coordinates([0,0],
+                       [0,1],
+                       directions.map {|s| s.gsub(/ +/, "")})
+
+    coord = path.pop
+    coord[0].abs + coord[1].abs 
+  end
+  
+  # given the current direction and the letter of the turn ,
+  # returns the new direction
+  def self.next_direction(currently_facing, direction_to_turn)
+    directions = [[0,1],[1,0],[0,-1],[-1,0]]
+    offset = (direction_to_turn == "R" ? 1 : -1)
+    directions[(directions.find_index(currently_facing) + offset) % 4]
+  end
+
+  # returns ALL coordinates - including in between ones
+  def self.coordinates(origin, currently_facing, remaining_directions, visited_locations = nil)
+    
+    # base case
+    if remaining_directions.empty?
+      return []
+    end
+
+    # recursive algo
+    current_direction = remaining_directions.shift
+    direction_to_turn = current_direction.slice!(0)
+    number_of_spaces = current_direction
+    new_facing = next_direction(currently_facing, direction_to_turn)
+
+    new_coordinates = number_of_spaces.to_i.times.map do |i|
+      [ origin[0] + (new_facing[0] * (i+1)) , origin[1] + (new_facing[1] * (i+1)) ]
+    end
+    
+#    puts "#{direction_to_turn}\t#{number_of_spaces}\t#{new_coordinates.inspect}"
+
+    new_coordinates + coordinates(new_coordinates.last, new_facing, remaining_directions)
   end
 
   def self.main
-    input = CSV.read("inputs/day1")
-    puts Day1.shortest_path(input)
+    input = CSV.read("inputs/day1.txt").flatten
+    puts "Part 1 : #{Day1.shortest_path_from_origin(input)}"
+    puts "Part 2 : #{Day1.shortest_path_to_first_place_to_visit_twice(input)}"
   end
 end
+
+Day1.main
+
--- spec/day1.rb
+++ spec/day1.rb
@@ -3,16 +3,29 @@ require './app/day1'
 
 describe Day1 do
 
-  it "ex 1" do
-    expect(Day1.shortest_path(["R2","L3"])).to eq(5)
+  describe "part 1 - shortest path to end" do
+    it "ex 1" do
+      expect(Day1.shortest_path_from_origin(["R2","L3"])).to eq(5)
+    end
+    
+    it "ex 2" do
+      expect(Day1.shortest_path_from_origin(["R2","R2","R2"])).to eq(2)
+    end
+    
+    it "ex 3" do
+      expect(Day1.shortest_path_from_origin(["R5","L5","R5","R3"])).to eq(12)
+    end
+    
+    it "multiple chars" do
+      expect(Day1.shortest_path_from_origin(["R5","L5","R50","R3"])).to eq(57)
+    end
   end
 
-  it "ex 2" do
-    expect(Day1.shortest_path(["R2","R2","R2"])).to eq(2)
-  end
+  describe "part 2 - first place to visit twice" do
 
-  it "ex 3" do
-    expect(Day1.shortest_path(["R5","L5","R5","R3"])).to eq(12)
+    it "ex 1" do
+      expect(Day1.shortest_path_to_first_place_to_visit_twice(["R8", "R4", "R4", "R8"])).to eq(4)
+    end
   end
 
 end
