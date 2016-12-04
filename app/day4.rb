d3a3dd5b5689e673647269240896f8cac464c059
Luke Shepard <luke@esparklearning.com>
2016-12-04 04:44:05 -0600

    Part 1 & Part 2 complete

--- app/day4.rb
+++ app/day4.rb
@@ -21,28 +21,47 @@
 #
 # What is the sum of the sector IDs of the real rooms?
 
+#
+# --- Part Two ---
+#
+# With all the decoy data out of the way, it's time to decrypt this list and get moving.
+#
+# The room names are encrypted by a state-of-the-art shift cipher, which is nearly
+# unbreakable without the right software. However, the information kiosk designers at 
+# Easter Bunny HQ were not expecting to deal with a master cryptographer like yourself.
+#
+# To decrypt a room name, rotate each letter forward through the alphabet a number of
+# times equal to the room's sector ID. A becomes B, B becomes C, Z becomes A, and so on.
+#  Dashes become spaces.
+#
+# For example, the real name for qzmt-zixmtkozy-ivhz-343 is very encrypted name.
+#
+# What is the sector ID of the room where North Pole objects are stored?
+
 class Day4
 
   def self.sum_of_sector_ids_of_real_rooms(input)
     input.split("\n").map do |line|
       data = split_room_line(line)
-      if checksum_correct?(data)
-        data[:sector_id].to_i
-      else
-        0
-      end
+    end.select do |data|
+      calculate_checksum(data[:encrypted_name]) == data[:checksum]
+    end.map do |data|
+      data[:sector_id].to_i
     end.reduce(&:+)
   end
 
+  def self.sector_id_the_special_room(input, match_phrase)
+    input.split("\n").map do |line| 
+      split_room_line(line)
+    end.select do |data|
+      name =  decrypt_name(data[:encrypted_name], data[:sector_id].to_i)
+      name == match_phrase
+    end.first[:sector_id]
+  end
 
   # A room is real (not a decoy) if the checksum is the five most common letters in the
   # encrypted name, in order, with ties broken by alphabetization.
-  def self.checksum_correct?(data)
-    # five most common letters
-    five_most_common_letters(data[:encrypted_name]) == data[:checksum]
-  end
-  
-  def self.five_most_common_letters(name)
+  def self.calculate_checksum(name)
     name
       .gsub(/-/,'')
       .chars
@@ -63,8 +82,24 @@ class Day4
     }
   end
   
+  # To decrypt a room name, rotate each letter forward through the alphabet a number of
+  # times equal to the room's sector ID. A becomes B, B becomes C, Z becomes A, and so on.
+  #  Dashes become spaces.
+  def self.decrypt_name(name, rotate_count)
+    name.chars.map do |char|
+      if char == "-"
+        " "
+      else
+        ((((char.ord - "a".ord) + rotate_count) % 26) + "a".ord).chr
+      end
+    end.join("")
+  end
+
   def self.main
     input = File.read("inputs/day4.txt")
     puts "Part 1: #{sum_of_sector_ids_of_real_rooms(input)}"
+    puts "Part 2: #{sector_id_the_special_room(input, 'northpole object storage')}"
   end
 end
+
+Day4.main
--- spec/day4.rb
+++ spec/day4.rb
@@ -3,14 +3,7 @@ require './app/day4'
 
 describe Day4 do
 
-  describe "part 1" do
-
-    
-    
-  end
-
   describe "split_room_line" do
-    
     it "first ex" do
       expect(Day4.split_room_line("aaaaa-bbb-z-y-x-123[abxyz]")
              ).to eq({encrypted_name: "aaaaa-bbb-z-y-x",
@@ -19,43 +12,33 @@ describe Day4 do
     end
   end
 
-  describe "five_most_common_letters" do
+  describe "calculate_checksum" do
     it "simple" do
-      expect(Day4.five_most_common_letters("aa-bb-cc-dd-ee")).to eq("abcde")
+      expect(Day4.calculate_checksum("aa-bb-cc-dd-ee")).to eq("abcde")
     end
 
     it "takes only 5" do
-      expect(Day4.five_most_common_letters("f-g-aa-bb-cc-dd-ee")).to eq("abcde")
+      expect(Day4.calculate_checksum("f-g-aa-bb-cc-dd-ee")).to eq("abcde")
     end
 
     it "breaks tie by alphabetization" do
-      expect(Day4.five_most_common_letters("ff-g-aaa-bbb-cc-dd-ee")).to eq("abcde")
+      expect(Day4.calculate_checksum("ff-g-aaa-bbb-cc-dd-ee")).to eq("abcde")
     end
-  end
 
-  describe "checksum correct" do
     it "ex 1" do
-      expect(Day4.checksum_correct?({encrypted_name: "aaaaa-bbb-z-y-x",
-                                      sector_id: 123,
-                                      checksum: "abxyz"})).to be_truthy
+      expect(Day4.calculate_checksum("aaaaa-bbb-z-y-x")).to eq("abxyz")
     end
     
     it "ex 2" do
-      expect(Day4.checksum_correct?({encrypted_name: "a-b-c-d-e-f-g-h",
-                                      sector_id: 987,
-                                      checksum: "abcde"})).to be_truthy      
+      expect(Day4.calculate_checksum("a-b-c-d-e-f-g-h")).to eq("abcde")
     end
 
     it "ex 3" do
-      expect(Day4.checksum_correct?({encrypted_name: "not-a-real-room",
-                                      sector_id: 404,
-                                      checksum: "oarel"})).to be_truthy
+      expect(Day4.calculate_checksum("not-a-real-room")).to eq("oarel")
     end
 
     it "ex 4" do
-      expect(Day4.checksum_correct?({encrypted_name: "totally-real-room",
-                                      sector_id: 200,
-                                      checksum: "decoy"})).to be_falsey
+      expect(Day4.calculate_checksum("totally-real-room")).not_to eq("decoy")
     end
   end
 
@@ -67,6 +50,12 @@ describe Day4 do
     
     it "matches given example" do
       expect(Day4.sum_of_sector_ids_of_real_rooms(input)).to eq(1514)
+    end
+  end
+  
+  describe "decrypt name" do
+    it "ex 1" do
+      expect(Day4.decrypt_name("qzmt-zixmtkozy-ivhz", 343)).to eq("very encrypted name")
     end
   end
 end
