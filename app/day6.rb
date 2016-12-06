ed9e333ff26d3e3bf2f13201e6887adb8c5a0cbe
Luke Shepard <luke@esparklearning.com>
2016-12-05 23:44:09 -0600

    Add comments and slight refactor for commonality between the parts

--- app/day6.rb
+++ app/day6.rb
@@ -1,16 +1,45 @@
+# --- Day 6: Signals and Noise ---
+#
+# Something is jamming your communications with Santa. Fortunately, your signal is
+# only partially jammed, and protocol in situations like this is to switch to a
+# simple repetition code to get the message through.
+
+# In this model, the same message is sent repeatedly. You've recorded the repeating
+# message signal (your puzzle input), but the data seems quite corrupted - almost too 
+# badly to recover. Almost.
+#
 class Day6
 
+  # All you need to do is figure out which character is most frequent for each position.
+  # The most common character in the first column is e; in the second, a; 
+  # in the third, s, and so on. Combining these characters returns the 
+  # error-corrected message, easter.
   def self.error_corrected_message(input)
-    input.split("\n").map {|row| row.strip.split(//)}.transpose.map do |column|
-      counts = column.inject(Hash.new(0)) { |h, x| h[x] += 1; h}
-      column.max_by { |v| counts[v] }
-    end.join("")
+    transpose_and_count(input, :max_by)
+  end
+  
+  #
+  # --- Part Two ---
+  # 
+  # Of course, that would be the message - if you hadn't agreed to use a modified
+  # repetition code instead.
+  #
+  # In this modified code, the sender instead transmits what looks like random 
+  # data, but for each character, the character they actually want to send is 
+  # slightly less likely than the others. Even after signal-jamming noise, you can
+  # look at the letter distributions in each column and choose the least common 
+  # letter to reconstruct the original message.
+  # 
+  def self.modified_repetition_error_corrected_message(input)
+    transpose_and_count(input, :min_by)
   end
 
-  def self.modified_repetition_error_corrected_message(input)
+  # Takes a newline-delimited series of strings, then transposes them by columns,
+  # evaluates each column according to the column_eval_fn, then joins the results
+  def self.transpose_and_count(input, column_eval_fn)
     input.split("\n").map {|row| row.strip.split(//)}.transpose.map do |column|
       counts = column.inject(Hash.new(0)) { |h, x| h[x] += 1; h}
-      column.min_by { |v| counts[v] }
+      column.send(column_eval_fn, &lambda {|v| counts[v]})
     end.join("")
   end
 
--- spec/day6.rb
+++ spec/day6.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 require './app/day6'
 
+
+
+
 describe Day6 do
 
   let(:sample_input) {"eedadn
@@ -22,6 +25,30 @@ enarar"}
 
   describe "part 1" do
     
+    #
+    # For example, suppose you had recorded the following messages:
+    #
+    # eedadn
+    # drvtee
+    # eandsr
+    # raavrd
+    # atevrs
+    # tsrnev
+    # sdttsa
+    # rasrtv
+    # nssdts
+    # ntnada
+    # svetve
+    # tesnvt
+    # vntsnd
+    # vrdear
+    # dvrsen
+    # enarar
+    # 
+    # The most common character in the first column is e; in the second, a; 
+    # in the third, s, and so on. Combining these characters returns the 
+    # error-corrected message, easter.
+
     it "sample input works" do
       expect(Day6.error_corrected_message(sample_input)).to eq("easter")
     end
@@ -29,6 +56,11 @@ enarar"}
   end
 
   describe "part 2" do
+
+    # In the above example, the least common character in the first column is a; 
+    # in the second, d, and so on. Repeating this process for the remaining 
+    # characters produces the original message, advent.
+    
     it "sample input works" do
       expect(Day6.modified_repetition_error_corrected_message(sample_input)).to eq("advent")
     end
