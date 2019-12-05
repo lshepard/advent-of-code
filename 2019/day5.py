2246148d1fcedbcb19d247763106b491642a2a2a
Luke Shepard <luke@lukeshepard.com>
2019-12-05 00:07:45 -0600

    omg that day 2

--- 2019/day5.py
+++ 2019/day5.py
@@ -9,7 +9,7 @@
 
 
 # Once you're done processing an opcode, move to the next one by stepping forward 4 positions.
-THE_INPUT = 1
+THE_INPUT = 5
 
 def parameter_mode(opcodes, n):
     s = str(opcodes)
@@ -45,7 +45,7 @@ def compute(input, noun=None, verb=None):
         # last 2 chars
         opcode = int(opcodes[-2:])
         
-        print(f"i:{i} opcodes:{opcodes} opcode:{opcode} outputs:{outputs} p224:{inputs[224:225]} inputs:{inputs}")
+        print(f"i:{i} opcodes:{opcodes} opcode:{opcode} outputs:{outputs}")
         
         # Opcode 1 adds together numbers read from two positions and stores
         # the result in a third position. The three integers immediately
@@ -55,7 +55,7 @@ def compute(input, noun=None, verb=None):
         if opcode == 1:
             p1 = value(inputs, i+1, parameter_mode(opcodes, 1))
             p2 = value(inputs, i+2, parameter_mode(opcodes, 2))
-
+            print(f"p1:{p1} p2:{p2}")
             output = p1 + p2
             inputs[inputs[i+3]] = output
             i += 4
@@ -66,9 +66,8 @@ def compute(input, noun=None, verb=None):
         elif opcode == 2:
             p1 = value(inputs, i+1, parameter_mode(opcodes, 1))
             p2 = value(inputs, i+2, parameter_mode(opcodes, 2))
-            p3 = value(inputs, i+2, parameter_mode(opcodes, 2))
+            print(f"p1:{p1} p2:{p2}")
 
-            print(f"opcode:2 p1{p1} p2{p2}")
             output = p1 * p2
             inputs[inputs[i+3]] = output
             i += 4
@@ -80,9 +79,51 @@ def compute(input, noun=None, verb=None):
             
         elif opcode == 4:
             p1 = value(inputs, i+1, parameter_mode(opcodes, 1))
+            print(f"p1:{p1}")
             outputs.append(p1)
             i += 2
-                
+
+        elif opcode == 5:
+            p1 = value(inputs, i+1, parameter_mode(opcodes, 1))
+            p2 = value(inputs, i+2, parameter_mode(opcodes, 2))
+            print(f"p1:{p1} p2:{p2}")
+            if p1 != 0:
+                i = p2
+            else:
+                i += 3
+
+        elif opcode == 6:
+            p1 = value(inputs, i+1, parameter_mode(opcodes, 1))
+            p2 = value(inputs, i+2, parameter_mode(opcodes, 2))
+            print(f"p1:{p1} p2:{p2}")
+            if p1 == 0:
+                i = p2
+            else:
+                i += 3
+
+        elif opcode == 7: # less than
+            p1 = value(inputs, i+1, parameter_mode(opcodes, 1))
+            p2 = value(inputs, i+2, parameter_mode(opcodes, 2))
+            print(f"p1:{p1} p2:{p2}")
+            
+            if p1 < p2:
+                inputs[inputs[i+3]] = 1
+            else:
+                inputs[inputs[i+3]] = 0
+            i += 4
+
+        elif opcode == 8: # less than
+            p1 = value(inputs, i+1, parameter_mode(opcodes, 1))
+            p2 = value(inputs, i+2, parameter_mode(opcodes, 2))
+            print(f"p1:{p1} p2:{p2}")
+            
+            if p1 == p2:
+                inputs[inputs[i+3]] = 1
+            else:
+                inputs[inputs[i+3]] = 0
+            i += 4
+
+            
         # 99 means that the program is finished and should immediately halt.
         elif opcode == 99:
             print(outputs)
@@ -100,11 +141,11 @@ def compute(input, noun=None, verb=None):
 assert(parameter_mode("1001",1) == 0)
 assert(parameter_mode("1001",2) == 1)
 
-
 # my provided input
 input = "3,225,1,225,6,6,1100,1,238,225,104,0,1102,7,85,225,1102,67,12,225,102,36,65,224,1001,224,-3096,224,4,224,1002,223,8,223,101,4,224,224,1,224,223,223,1001,17,31,224,1001,224,-98,224,4,224,1002,223,8,223,101,5,224,224,1,223,224,223,1101,86,19,225,1101,5,27,225,1102,18,37,225,2,125,74,224,1001,224,-1406,224,4,224,102,8,223,223,101,2,224,224,1,224,223,223,1102,13,47,225,1,99,14,224,1001,224,-98,224,4,224,102,8,223,223,1001,224,2,224,1,224,223,223,1101,38,88,225,1102,91,36,224,101,-3276,224,224,4,224,1002,223,8,223,101,3,224,224,1,224,223,223,1101,59,76,224,1001,224,-135,224,4,224,102,8,223,223,1001,224,6,224,1,223,224,223,101,90,195,224,1001,224,-112,224,4,224,102,8,223,223,1001,224,7,224,1,224,223,223,1102,22,28,225,1002,69,47,224,1001,224,-235,224,4,224,1002,223,8,223,101,5,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,107,226,226,224,102,2,223,223,1006,224,329,1001,223,1,223,1107,677,226,224,1002,223,2,223,1005,224,344,101,1,223,223,108,677,226,224,102,2,223,223,1006,224,359,101,1,223,223,7,677,226,224,102,2,223,223,1005,224,374,101,1,223,223,1008,677,226,224,1002,223,2,223,1006,224,389,1001,223,1,223,7,226,677,224,102,2,223,223,1005,224,404,101,1,223,223,1007,226,226,224,102,2,223,223,1006,224,419,101,1,223,223,7,226,226,224,102,2,223,223,1005,224,434,1001,223,1,223,8,226,226,224,1002,223,2,223,1006,224,449,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,464,101,1,223,223,1007,226,677,224,1002,223,2,223,1006,224,479,101,1,223,223,108,226,226,224,102,2,223,223,1005,224,494,1001,223,1,223,1108,677,677,224,102,2,223,223,1005,224,509,1001,223,1,223,107,226,677,224,1002,223,2,223,1005,224,524,101,1,223,223,1108,677,226,224,1002,223,2,223,1005,224,539,1001,223,1,223,1008,677,677,224,1002,223,2,223,1006,224,554,101,1,223,223,1008,226,226,224,102,2,223,223,1005,224,569,1001,223,1,223,8,677,226,224,102,2,223,223,1006,224,584,101,1,223,223,107,677,677,224,102,2,223,223,1006,224,599,101,1,223,223,8,226,677,224,102,2,223,223,1006,224,614,101,1,223,223,1107,226,677,224,102,2,223,223,1006,224,629,101,1,223,223,108,677,677,224,1002,223,2,223,1005,224,644,1001,223,1,223,1107,226,226,224,102,2,223,223,1005,224,659,101,1,223,223,1108,226,677,224,102,2,223,223,1005,224,674,101,1,223,223,4,223,99,226"
 
-assert(compute("3,0,4,0,99") == "1")
+assert(compute("3,0,4,0,99") == "5")
+assert(compute("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99") == "999")
 
 # Part 1:
 print("Part 1:")
