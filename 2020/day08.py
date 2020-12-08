d8dc4fd569724da913da61f900988fc919de42b5
Luke Shepard <luke@lukeshepard.com>
2020-12-07 23:21:38 -0600

    Try changes

--- 2020/day08.py
+++ 2020/day08.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 
 test = """nop +0
 acc +1
@@ -7,8 +8,7 @@ jmp -3
 acc -99
 acc +1
 jmp -4
-acc +6
-"""
+acc +6"""
 
 
 test_fixed = """nop +0
@@ -18,7 +18,7 @@ acc +3
 jmp -3
 acc -99
 acc +1
-jmp -4
+nop -4
 acc +6
 """
 
@@ -27,12 +27,13 @@ def process_code(code):
     line = 0
     lines_seen = set()
     while(True):
-        if line in lines_seen:
-            print("Last line " + str(line))
-            raise ValueError("last line")
-        
         if line >= len(code):
+            #print("hit on the line")
             return accumulator
+
+        if line in lines_seen:
+            return None # didn't work
+
         lines_seen.add(line)
 
         inst, val = code[line].split(" ")
@@ -47,11 +48,40 @@ def process_code(code):
         else:
             raise ValueError("Invalid instruction " + inst)
 
-        print(inst, val, accumulator)
+#        print(inst, val, accumulator)
 
 
-print(process_code(test.split("\n")))
+def try_changes(code):
+    for i, row in tqdm(enumerate(code)):
+        r = try_change(code, i)
+        if r is not None:
+            return r
+        
+    # going to try each of the changes
+
+
+def try_change(code, line):
+    # change line x of the code and try it out
+    #print("changing ",line)
+    #print("changing ",code[line])
+    inst, val = code[line].split(" ")
+    c2 = code.copy()
+    if inst == "jmp":
+        c2[line] = "nop " + val
+        return process_code(c2)
+    elif inst == "nop":
+        c2[line] = "jmp " + val
+        return process_code(c2)
+    else:
+        return None
+    
+        
+    
+#print(process_code(test.split("\n")))
 
 #inp = open('inputs/day07.test').read().strip().split("\n")
 inp = open('inputs/day08').readlines()
-print(process_code(inp))
+#print(process_code(inp))
+
+#print(try_changes(test.split("\n")))
+print(try_changes(inp))
