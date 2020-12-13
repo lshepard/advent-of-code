48e1cb8efe69db54c10660e16c33a212e508048a
Luke Shepard <luke.shepard@tempus.com>
2020-12-13 10:53:40 -0600

    figured it out! worked incrementally thruogh each number, able to make it up quickly :)

--- 2020/day13.py
+++ 2020/day13.py
@@ -1,3 +1,4 @@
+import math
 import fileinput
 
 lines = list(fileinput.input())
@@ -22,24 +23,42 @@ def earliest_dept(earliest, bus_strs):
     return m * next_times[m]
 
 def earliest_sequential(bus_strs):
-    buses = [int(num) for num in bus_strs.split(",")]
+    buses = dict((int(num), i) for i, num in enumerate(bus_strs.split(",")) if num != 'x')
 
-
-    equation is :
-
-    ni =
-    
-    next_times = dict()
     print(buses)
-    for bus in buses:
-        rem = earliest % bus
-        if rem == 0:
-            return bus
-        else:
-            next_times[bus] = bus - rem
 
-    m = min(next_times, key=next_times.get)
-    return m * next_times[m]
+    # going to accelerate, looking first for the next biggest
+    num = 1
+    increment = 1
+    
+    print(buses.items())
+    for bus, offset in sorted(buses.items(), reverse=True):
+        print("bus",bus,"offset",offset,"num",num,"increment", increment)
+        while((num + offset) % bus != 0):
+            bus_checks = [ (b, (num + offset) % b) for b, offset in buses.items()]
+            print(num, bus_checks)
+            num += increment
 
+        # found one that satisfies this
+        increment = increment * bus
 
-print(earliest_dept(earliest, bus_strs))
+    return num
+
+    print(bus, offset)
+    
+    num_digits = 0
+    while True:
+        i += max_bus
+        if num_digits < int(math.log(i,10)):
+            num_digits = int(math.log(i,10))
+            print(str(i) + " has " + str(num_digits+1) + " digits")
+        # check against all the others
+        bus_checks = [ (i + offset) % bus == 0 for bus, offset in buses.items()]
+        #if (i>1068000): # 1068781
+        #    print(i, bus_checks)
+        if all(bus_checks):
+            return i
+
+        
+
+print(earliest_sequential(bus_strs))
