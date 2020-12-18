88627948e368e1155bc7fcd34393d4858d648bf2
Luke Shepard <luke.shepard@tempus.com>
2020-12-18 00:16:37 -0600

    partially working - but wrong answer

--- 2020/day18.py
+++ 2020/day18.py
@@ -1,13 +1,45 @@
 import parser
 import re
 
-def eval2(formula, nestdepth=0):
+def ev(formula):
+    print("------ " + formula)
+    return eval2(formula)
+
+def replace_pluses(formula):
+    """replace the pluses"""
+    matches = re.search(r"\d+ \+ \d+", formula)
+    if matches is None:
+        return eval(formula)
+    else:
+        formula = formula[:m.start()] + replace_pluses(matches[0]) + formula[m.end():]
+    
+
+    
+def eval2(formula):
     formula = formula.strip()
-    print("rewritten", formula, nestdepth)
     
     """ takes a line and rewrites it , rewriting subsequences as well """
+    # if there aren't any parentheses in this ...
+
+    if formula.find("(") == None:
+        # then replace all the +s
+
+        
     
-    # pull out subseqeuences within 
+    # start by pulling out all the +s
+    matches = re.findall(r' (\d+ \+ \d+) ', formula)
+    while len(matches) > 0:
+#        print(matches)
+        for m in matches:
+            # just evaluate this one directly
+            code = parser.expr(m).compile()
+            val = eval(code)
+
+            formula = formula.replace(m, str(val)) # replace it in the string
+        matches = re.findall(r' (\d+ \+ \d+) ', formula)
+        print(formula)
+    
+    # pull out subseqeuences within and replace with their values calculated
     matches = re.findall(r'\(([^()]+)\)', formula)
     while len(matches) > 0:
         #print(matches, nestdepth)
@@ -15,32 +47,24 @@ def eval2(formula, nestdepth=0):
             val = eval2(m)
             formula = formula.replace("("+m+")", str(val)) # replace it in the string
         matches = re.findall(r'\(([^()]+)\)', formula)
-    
+        print(formula)
+    # then pull out the addition and do that
 
-    # now that there are no nested parens, add the parens inside
-
-    # rewrite the line
-    f2 = ""
-    c = 0
-    for term in formula.split(" "):
-        
-        if term in ["*","+"]:
-            f2 += ")"
-            c += 1
-        f2 += term + " "
-
-    f2 = "(" * c + f2
-    code = parser.expr(f2).compile()
+    code = parser.expr(formula).compile()
     return eval(code)
 
 #f = "1 + 2 * 3 + 4 * 5 + 6"
 f = "2 * 3 + (4 * 5)"
 print(eval2(f))
+lines = ["1 + 2 * 3 + 4 * 5 + 6 ","1 + (2 * 3) + (4 * (5 + 6))",
+         "2 * 3 + (4 * 5)", "5 + (8 * 3 + 9 + 3 * 4 * 3)",
+         "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)) ", "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"]
 lines = open("inputs/day18").readlines()
-#lines = ["1 + (2 * 3) + (4 * (5 + 6))",
-#         "2 * 3 + (4 * 5)", "5 + (8 * 3 + 9 + 3 * 4 * 3)",
-#         "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)) ", "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"]
 
-for line in lines:
-    print("orig:",line,"rewrite",eval2(line))
-print(sum([int(eval2(line.strip())) for line in lines]))
+#for line in lines:
+#    print(eval2(line),"orig:",line.strip(),)
+    
+print("\n".join([str(ev(line.strip())) + "\to:" + line.strip() for line in lines]))
+vals = [int(ev(line.strip())) for line in lines]
+print(len(vals))
+print(sum(vals))
