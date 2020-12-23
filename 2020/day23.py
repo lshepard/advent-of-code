79f66685d6c09caf70f974fb4516c619a0c40a49
Luke Shepard <luke.shepard@tempus.com>
2020-12-23 10:41:46 -0600

    man had to do a linked list

--- 2020/day23.py
+++ 2020/day23.py
@@ -1,51 +1,92 @@
 inp = """284573961"""
+from tqdm import tqdm
 
-def next_move(cups):
-    """Perform actions for each move, given a list of cups."""
+def create_list(cups_string, max_len=None):
+   nums = [int(i) for i in list(cups_string)]
+   if max_len:
+       nums += list(range(max(nums)+1, max_len+1))
+   nodes = dict()
 
-    current = cups[0]
+   first = nums[0]
+   prev = None
+   for i in nums:
+       if prev:
+           nodes[prev] = i
+       prev = i
+
+   # finally, link the last one to the first one so it's a cycle
+   nodes[prev] = first
+
+   return nodes
+
+def nodes_str(nodes, start_index):
+    n = start_index
+    s = str(n)
+    for i in range(1,len(nodes)):
+        n = nodes[n]
+        s += "," + str(n)
+    return s
+        
+def moves(nodes, max_length, num_moves, start):
+
+    current = start
+    for x in tqdm(range(num_moves)):
+        #print(nodes)
+        #print(nodes_str(nodes, current))
+        
+        # The crab picks up the three cups that are immediately clockwise of the current cup.
+        p1 = nodes[current]
+        p2 = nodes[p1]
+        p3 = nodes[p2]
+        picked = set([p1, p2, p3])
+
+        #print("picked", [p1,p2,p3])
+        # They are removed from the circle; cup spacing is adjusted as necessary to maintain the circle.
+        nodes[current] = nodes[p3]
+        nodes[p3] = None
+        #print(nodes)
+        
+        # The crab selects a destination cup: the cup with a label equal to the current cup's label minus one.
+        di = None
+        for i in range(1, max_length):
+            di = (current - i - 1) % max_length + 1
+            #print("---testing " + str(di))
+            if di not in picked:
+                #print(" -- cant choose " + str(di) + " its in picked")
+                break
+        #print("chose " + str(di))    
+            
+        # The crab places the cups it just picked up so that they are immediately clockwise
+        # of the destination cup. They keep the same order as when they were picked up.
+        #print("destination", di)
+        #print("setting p3 ", p3 , "to ", nodes[di])
+        nodes[p3] = nodes[di]
+        nodes[di] = p1
+        #print("before choosing" , nodes, "current still ", current, "next current", nodes[current])    
+        # The crab selects a new current cup: the cup which is immediately clockwise of the current cup.
+        current = nodes[current]
+            
+    return (nodes, current)
+
+if False:
+    part1_nodes = create_list("389125467")
+    (nodes, current_index) = moves(part1_nodes, len(part1_nodes), 10, start=3)
     
-    # The crab picks up the three cups that are immediately clockwise of the current cup.
-    # They are removed from the circle; cup spacing is adjusted as necessary to maintain the circle.
-    picked_up = cups[1:4]
-    remaining = [ cups[0] ] + cups[4:]
-
-    # The crab selects a destination cup: the cup with a label equal to the current cup's label minus one.
-    destination_i = None
-    i = 1
-    while True:
-        # If at any point in this process the value goes below the lowest value on any cup's label,
-        # it wraps around to the highest value on any cup's label instead.
-
-        try:
-            # If this would select one of the cups that was just picked up, the crab will keep subtracting one
-            # until it finds a cup that wasn't just picked up.
-            destination_i = remaining.index((current - i-1) % len(cups) + 1)
-            break
-        except:
-            i += 1
-            next
-
-    print("destination: " + str(remaining[destination_i]))
-    
-    # The crab places the cups it just picked up so that they are immediately clockwise
-    # of the destination cup. They keep the same order as when they were picked up.
-    # The crab selects a new current cup: the cup which is immediately clockwise of the current cup.
-
-    # new order: the three picked up, then the remaining items up to current cup, then the rest up to the destination cup
-    return remaining[1:destination_i+1] + picked_up + remaining[destination_i+1:] + [ remaining[0] ]
+    # After the crab is done, what order will the cups be in?
+    # Starting after the cup labeled 1, collect the other cups' labels clockwise
+    # into a single string with no extra characters; each number except 1 should
+    # appear exactly once.
+    s = nodes_str(nodes, 1)
+    print("Part 1", s[:-1])
 
 
-cups = [int(i) for i in list("389125467")]
-cups = [int(i) for i in list(inp)]
-for i in range(100):
-    print(cups)
-    cups = next_move(cups)
+# now on to part 2
 
-# After the crab is done, what order will the cups be in?
-# Starting after the cup labeled 1, collect the other cups' labels clockwise
-# into a single string with no extra characters; each number except 1 should
-# appear exactly once.
-
-one_index = cups.index(1)
-print("".join([str(c) for c in  cups[one_index+1:] + cups[:one_index]]))
+#part2_nodes = create_list("389125467", 1_000_000)
+part2_nodes = create_list(inp, 1_000_000)
+print(nodes_str(part2_nodes,2))
+(nodes, current_index) = moves(part2_nodes, len(part2_nodes), 10_000_000, start=2)
+p1 = nodes[1]
+p2 = nodes[p1]
+print(p1, p2)
+print(p1 * p2)
