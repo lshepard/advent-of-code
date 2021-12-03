c15d509cbd63b2bd723e0ff4936eecab3ae525f1
Luke Shepard <luke@lukeshepard.com>
2021-12-02 23:54:45 -0600

    what a mess

--- 2021/day03.py
+++ 2021/day03.py
@@ -2,24 +2,58 @@ import re
 import fileinput
 import pandas as pd
 
+lines = """00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010""".split("\n")
 lines = list(fileinput.input())
 
-df = pd.DataFrame([list(line.strip()) for line in lines])
+# first calculate the numbers
 
-m = df.mode()
 
-print(m)
-m = "10110"
-m = "010111100100"
-gamma = ""
-epsilon = ""
-for i in list(m):
-    print("i",i)
-    e = (int(i) + 1) % 2
-    print("e",e)
-    gamma = gamma + str(int(i))
-    epsilon = epsilon + str(e)
-    
-print(int(gamma,2))
-print(int(epsilon,2))
-print(int(gamma,2) * int(epsilon,2))
+def mode(nums, position):
+    """Calculate the number most prevalent at the position for a set of num strings"""
+    total = len(nums)
+    one_count = len([num for num in nums if num[position] == "1"])
+    print("evaluating for position", position, "ones", one_count, "total" , total, "for nums", nums)
+    if one_count >= total/2:
+        return "1"
+    else:
+        return "0"
+
+
+nums = [line.strip() for line in lines]
+print(nums)
+
+def rating(nums, swap_comparison=False):
+    for i in range(0,12):
+        comparison = mode(nums, i)
+        print("position ", i, "comparison", comparison)
+        if swap_comparison:
+            comparison = str((int(comparison) + 1) % 2)
+        swapped = [num for num in nums if num[i] == comparison]
+        print(i, swapped)
+        if len(swapped) == 1:
+            num = swapped[0]
+            break
+        elif len(swapped) == 0:
+            print("last item", nums)
+            num = nums[-1]
+            break
+        nums = swapped
+    return int(num,2)
+
+a = rating(nums, False)
+b =rating(nums, True)
+print (a * b)
+
+#a = num
+#print(int(a) * int(b))
