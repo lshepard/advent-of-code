7d199e4fd234474b08e2029e79485406d4602ead
Luke Shepard <luke@lukeshepard.com>
2021-12-07 05:41:05 -0600

    Parts 1 and 2 of day 7

--- 2021/day07-crab-distance.py
+++ 2021/day07-crab-distance.py
@@ -1,20 +1,27 @@
 import fileinput
+from bisect import bisect_left
 
-def cheapest_cost(inp):
+def sum_of_numbers(n):
+    return (n / 2) * (1 + n)
+    
+def cheapest_cost(inp, cost_fn):
     crabs = [int(n) for n in inp.split(",")]
-
-    
-    
-    position_costs = dict((position, cost(crabs, position)) for position in range(min(crabs), max(crabs)))
-    print(position_costs)
+    position_costs = dict((position, cost_fn(crabs, position)) for position in range(min(crabs), max(crabs)))
     min_position = min(position_costs, key=position_costs.get)
     return position_costs[min_position]
 
-def cost(crabs, end_position):
+def one_cost(crabs, end_position):
     return sum([ abs(crab - end_position) for crab in crabs])
 
-assert(cheapest_cost("16,1,2,0,4,2,7,1,2,14") == 37)
+def higher_cost(crabs, end_position):
+    return sum([ sum_of_numbers(abs(crab - end_position)) for crab in crabs])
+
+assert(cheapest_cost("16,1,2,0,4,2,7,1,2,14", one_cost) == 37)
+assert(cheapest_cost("16,1,2,0,4,2,7,1,2,14", higher_cost) == 168)
 
 crabs = fileinput.input()[0].strip()
+print("Part 1")
+print(cheapest_cost(crabs, one_cost))
 
-print(cheapest_cost(crabs))
+print("Part 2")
+print(cheapest_cost(crabs, higher_cost))
