c0fe69d9220fc0693ee5af0540211f59566ad6aa
Luke Shepard <luke@lukeshepard.com>
2021-12-13 23:57:28 -0600

    that part 2 tho

--- 2021/day14.py
+++ 2021/day14.py
@@ -1,3 +1,4 @@
+from math import ceil
 import fileinput
 from collections import Counter
 import re
@@ -53,11 +54,58 @@ def part1(lines):
     counts = Counter(s)
     print(counts)
     return max(counts.values()) - min(counts.values())
-    
 
-#starter, maps, pattern = parse_input(test)
-#print(part1(test))
+def part2(lines):
+    starter = lines[0].strip()
+
+    maps = dict()
+    for line in lines[2:]:
+        left, right = line.strip().split(" -> ")
+
+        maps[left] = [left[0] + right, right + left[1]]
+
+    counts = dict()
+    print(f"starter {starter}")
+    for i in range(len(starter)-1):
+        c = starter[i:i+2]
+        counts[c] = counts.get(c, 0) + 1
+
+    # starting counts
+
+
+    print(counts)
+    for i in range(40):
+        new_counts = dict()
+        for item, count in counts.copy().items():
+            r1, r2 = maps[item]
+            new_counts[r1] = new_counts.get(r1, 0) + int(count)
+            new_counts[r2] = new_counts.get(r2, 0) + int(count)
+            
+        counts = new_counts
+        print(counts)
+
+        single_letter_counts = dict()
+        for pair, count in counts.items():
+            single_letter_counts[pair[0]] = single_letter_counts.get(pair[0], 0) + count
+            single_letter_counts[pair[1]] = single_letter_counts.get(pair[1], 0) + count
+            
+        print(f"single {single_letter_counts}")
+
+    # final single letter counts
+    single_letter_counts = dict()
+    for pair, count in counts.items():
+        single_letter_counts[pair[0]] = single_letter_counts.get(pair[0], 0) + count
+        single_letter_counts[pair[1]] = single_letter_counts.get(pair[1], 0) + count
+        
+
+    real_counts = dict( (single, ceil(count / 2)) for single, count in single_letter_counts.items() )
+
+    return max(real_counts.values()) - min(real_counts.values())
+    
+        
+print(part2(test))
+
 inp = list(fileinput.input())
-print(part1(inp))
+print(part2(inp))
 
 
