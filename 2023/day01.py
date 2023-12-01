d010f89bb57484ad3ce6a9b2b98fc0c5c27e75f8
Luke Shepard <luke@lukeshepard.com>
2023-12-01 14:09:53 -0600

    day1 in the books

--- 2023/day01.py
+++ 2023/day01.py
@@ -4,12 +4,18 @@ import fileinput
 lines = list(fileinput.input())
 
 
-def convert_to_nums(txt):
+nums = {"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4",
+       "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
+revs = dict(("".join(reversed(k)), v)  for k, v in nums.items())
+
+print(revs)
+            
+def convert_to_nums(txt, regexes):
     """Converts a number like 'abcone2threexyz' to 'abc123xyz'"""
 
-    rep = {"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4",
-           "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-    rep = dict((re.escape(k), v) for k, v in rep.items())
+    global rep
+    rep = dict((re.escape(k), v) for k, v in regexes.items())
     pattern = re.compile("|".join(rep.keys()))
     x = pattern.sub(lambda m: rep[re.escape(m.group(0))], txt)
     print(txt, x)
@@ -17,13 +23,17 @@ def convert_to_nums(txt):
 
 def firstlast(txt):
     """Gets the first/last digit and makes a number"""
+    global nums, revs
+    
+    # left side is easy
+    l = re.search("\d",convert_to_nums(txt, nums))[0]
+    r = re.search("\d",convert_to_nums("".join(reversed(txt)), revs))[0]
 
-    l = re.search("\d",txt)[0]
-    r = re.search("\d","".join(reversed(txt)))[0]
-    print("\t\t", txt,l,r)
+    # for the right one i need to search for the first instance of the phrase from the right
+     
     return int(l + r)
 
-nums = [firstlast(convert_to_nums(line.strip())) for line in lines]
+nums = [firstlast(line.strip()) for line in lines]
 
 print(len(nums))
 print(sum(nums))
