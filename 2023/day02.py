814f1520e339eac79557384edeac12fa238fdf72
Luke Shepard <luke@lukeshepard.com>
2023-12-02 06:43:37 -0600

    part1

---
+++ 2023/day02.py
@@ -0,0 +1,41 @@
+import re
+import fileinput
+
+
+def parse(line):
+    """Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red""" 
+
+    m = re.match("Game (\d+): (.*)", line.strip())
+    game_id = int(m[1])
+
+    sets = m[2].split("; ")
+    results = []
+    for set in sets:
+        choices = set.split(", ")
+
+        for c in choices:
+            m = re.match("(\d+) (green|red|blue)", c)
+            quantity, color = int(m[1]), m[2]
+            results.append([quantity, color])
+
+    return game_id, results
+
+
+def part1():
+    """ only 12 red cubes, 13 green cubes, and 14 blue cubes? """
+    lines = list(fileinput.input())
+    
+    n = 0
+    for line in lines:
+        game_id, set_results = parse(line)
+        if possible(set_results, {"red": 12, "green": 13, "blue": 14}):
+            print(f"Successful {game_id}")
+            n += game_id
+
+    print(n)
+
+def possible(set, comparison):
+    """Tells whether the set could work. comparision as form {"green": 2, "red": 3, "blue": 6}"""
+    return all([quantity <= comparison[color] for quantity, color in set])
+
+part1()
