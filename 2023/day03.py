1dda2a80eccacb3df7441e00acdc831ee014e2bc
Luke Shepard <luke@lukeshepard.com>
2023-12-03 05:41:38 -0600

    added in defaultdict, inspired https://topaz.github.io/paste/#XQAAAQA8AgAAAAAAAAA0m0pnuFI8c82uPD0wiI6r5tRTRja98xwzm8xiE97Z03X4hF0hajRBL/zAq5dtOiXZi4k9MbTVR01MJLPnDa9B3BU8M39zcTFhoa+g92wR8GPkr9qKRPPAZq5muUxTUGfJH/M2U4SW0VrcdguGTyGudhhIMahw8B1f2KpLcxcXL1OErr56Rm9JrG2RKtp+/wN0L3ozOPevv5TuCpIpuowUCPSYzrQoD9CGhQr0t52WH5uDVH5jULSb3D6yebuA/YE+PWENcvz9z/LU6vLB1lNqFz+5XO9GV19RhdHUqNdbAr5OZv4YqfgTkYdJH1M3yramzdf1xn9PWrbJKgYCwx48vLtxw3hthkikQXGtR0oguXkiERlfQPVy7JtQAV9mf1hmQ6APCmNE0dNe/LdGTI2zAnzCPTP/RGD4HO7ntk1zG9YJXhvhSHvrU9yLJ9//TUFfAA==

--- 2023/day03.py
+++ 2023/day03.py
@@ -1,18 +1,23 @@
 import math
 import re
 import fileinput
+import collections
 
 lines = list(fileinput.input())
 
+# this will help us avoid out of bounds errors
 all_points = set([(x,y) for x in range(len(lines)) for y in range(len(lines[0].strip()))])
 
 part_numbers = []
-gears = dict()
+gears = collections.defaultdict(list)
 
 for row, line in enumerate(lines):
     for m in re.finditer("(\d+)", line.strip()):
 
-        adjacents = [(i,j) for j in range(m.start()-1, m.end()+1) for i in [row-1,row,row+1] if i != row or j < m.start() or j >= m.end()]
+        adjacents = [(i,j) for j in range(m.start()-1, m.end()+1) 
+                     for i in [row-1,row,row+1] 
+                     if i != row or j < m.start() or j >= m.end()]
+        
         achars = [lines[i][j] for (i,j) in adjacents if (i,j) in all_points]
         num = int(line[m.start():m.end()])
         is_part_number = len(set(achars) - set(".")) > 0
@@ -22,11 +27,8 @@ for row, line in enumerate(lines):
         # log the gears for next part
         for a in adjacents:
             if (a in all_points) and (lines[a[0]][a[1]] == "*"):
-                gears[a] = gears.get(a,[])
-                gears[a].append(num)
+                gears[a].append(num) # defaultdict gives us a list automatically
                 
-#        print(row, num, m.start(), set(achars), adjacents)
-
 # part1
 print(f"Part 1 (sum of part numbers): {sum(part_numbers)}")
 
